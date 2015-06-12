3f8d3dcaebf5b6cefa4a6dced3f504eaa2686d4d
jiang <jiang@linyu.com>
2015-06-12 10:11:29 +0800

    AF_INET changed af_inet, avoiding macro definition

--- socket/socket_ns.h
+++ socket/socket_ns.h
@@ -1,32 +1,32 @@
 #ifndef SOCKET_NS_H
 #define SOCKET_NS_H
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
 #include <errno.h>
 
-#include <string>
+#include <cstring> 
+#include <utility> 
+#include <string> 
 #include <iostream>
+
 using std::string;
 using std::pair;
 using std::cout;
 using std::cerr;
 using std::endl;
-
-const int __AF_INET = AF_INET;
-#undef AF_INET
-const int AF_INET = __AF_INET;
+using std::memset;
 
 namespace socket_ns {
 
-const int AF_INET = ::AF_INET;
-const int SOCK_STREAM = ::SOCK_STREAM;
-const int SOCK_DGRAM = ::SOCK_DGRAM;
-const int BUFSIZE = 1024;
+const int af_inet = AF_INET;
+const int sock_stream = SOCK_STREAM;
+const int sock_dgram = SOCK_DGRAM;
+const int buf_size = 1024;
 
 typedef pair<string, int> Address;
 
 };
+
 #endif //SOCKET_NS_H
